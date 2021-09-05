5657827211bd7c07401e49febfdb0a542e90e4b2
ankitAalekh <ankitbokaro1996@gmail.com>
2021-09-05 19:17:55 +0530

    ready for deployment

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,63 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
+import { Fragment } from "react";
+import Head from "next/dist/next-server/lib/head";
+function MeetupDetails(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta
+          name="description"
+          content={props.meetupData.description}
+        />
+      </Head>
+      <MeetupDetail
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        image={props.meetupData.image}
+        description={props.meetupData.description}
+      />
+    </Fragment>
+  );
+}
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://kumar_ankit:WDn9kQvOoJmV0nUe@cluster0.zrjz1.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  const client = await MongoClient.connect(
+    "mongodb+srv://kumar_ankit:WDn9kQvOoJmV0nUe@cluster0.zrjz1.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,34 @@
+// our-domain.com/new-meetup/
+import { useRouter } from "next/router";
+import Head from "next/dist/next-server/lib/head";
+import { Fragment } from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+function NewMeetupPage() {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
+  }
+  return (
+    <Fragment>
+        <Head>
+        <title>Add a new meetup</title>
+        <meta
+          name="description"
+          content="Create your own meetups and create amazing oppurtunities!!"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+}
+
+export default NewMeetupPage;
